72025db695ee3382218ec4de8d77fa5c02e781bd
qsrdev <matteo.albertini747@gmail.com>
2025-04-16 16:46:22 +0200

    feat: minor fix

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -50,5 +50,7 @@ console.log(teachersString);
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
+
 const isTeachersEmpty = Boolean((teachers.length = 0));
+// const isTeachersEmpty = teachers.length === 0;
 console.log(isTeachersEmpty);
